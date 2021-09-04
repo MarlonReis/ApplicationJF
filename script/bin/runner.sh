11170689bbab0efa74f013d65b91a1f184613c7e
Marlon Reis <marlon-reis91@hotmail.com>
2021-09-04 19:20:10 -0300

    Adicionando script

---
+++ script/bin/runner.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+JFX_PATH=/opt/javafx-sdk-14.0.1/lib
+APPLICATION_LAST_VERSION=$(find $SNAP/jar/ -name "ApplicationUpdateMachine*.jar" -type f | sort -n | tail -n1)
+
+
+desktop-launch java -Duser.timezone='GMT-03' --module-path $JFX_PATH --add-modules javafx.base,javafx.graphics,javafx.media,javafx.controls,javafx.fxml -jar $APPLICATION_LAST_VERSION
