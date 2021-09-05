a4f0a0b91a8fde4bad5f8fbf9871f96d0bae2ff1
Marlon Reis <marlon-reis91@hotmail.com>
2021-09-05 19:16:54 -0300

    Altera o titulo

--- src/main/java/com/example/applicationupdatemachine/HelloApplication.java
+++ src/main/java/com/example/applicationupdatemachine/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Somente!");
+        stage.setTitle("Demonstracao!");
         stage.setScene(scene);
         stage.show();
     }
