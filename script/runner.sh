f20a85bbec5a024f70159cc1a13efb0f5fe7e199
Marlon Reis <marlon-reis91@hotmail.com>
2021-09-05 16:39:29 -0300

    Runner script

--- script/runner.sh
+++ script/runner.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-
-JFX_PATH=/opt/javafx-sdk-14.0.1/lib
-APPLICATION_LAST_VERSION=$(find . -name "ApplicationUpdateMachine*.jar" -type f | sort -n | tail -n1)
-
-
-desktop-launch java -Duser.timezone='GMT-03' --module-path $JFX_PATH --add-modules javafx.base,javafx.graphics,javafx.media,javafx.controls,javafx.fxml -jar $APPLICATION_LAST_VERSION
+desktop-launch java -Duser.timezone='GMT-03' --module-path $SNAP/usr/share/java/ --add-modules javafx.base,javafx.graphics,javafx.media,javafx.controls,javafx.fxml -jar $SNAP/jar/app-sp.jar
